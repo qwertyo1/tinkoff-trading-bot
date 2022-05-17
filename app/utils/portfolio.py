fe8f3691b9d17f01ea45f9ed1f43f218188e6373
Vladimir Klipert <vklipert@worldsensing.com>
2022-05-17 21:08:25 +0200

    Added utils for working with portfolio

---
+++ app/utils/portfolio.py
@@ -0,0 +1,31 @@
+from typing import List, Optional
+
+from tinkoff.invest import PortfolioPosition, OrderState
+
+
+def get_position(positions: List[PortfolioPosition], figi: str) -> Optional[PortfolioPosition]:
+    """
+    Find position by figi
+
+    :param positions: list of positions
+    :param figi: figi of position
+    :return: position
+    """
+    for position in positions:
+        if position.figi == figi:
+            return position
+    return None
+
+
+def get_order(orders: List[OrderState], figi: str) -> Optional[OrderState]:
+    """
+    Find position by figi
+
+    :param orders: list of orders
+    :param figi: figi of order
+    :return: position
+    """
+    for order in orders:
+        if order.figi == figi:
+            return order
+    return None
